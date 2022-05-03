11a9f355d2ebbb845f78635f8a4f4f2bfcbd5fbc
Shiba Koenaite <shiba.koenaite@dadevs.co.za>
2022-05-03 12:34:05 +0200

    make it better

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,26 +3,26 @@
 // The list of file replacements can be found in `angular.json`.
 
 //-------------------------------Heroku_URLs-------------------------------
-// const BaseUrl = 'https://edu-incpg.herokuapp.com/user/';
-// const assignedSubject = 'https://edu-incpg.herokuapp.com/assignedSubject/';
-// const Subject = 'https://edu-incpg.herokuapp.com/subject/';
-// const authentication = 'https://edu-incpg.herokuapp.com/auth/';
-// const docUpload = 'https://edu-incpg.herokuapp.com/upload/';
-// const messaging = 'https://edu-incpg.herokuapp.com/messaging/';
-// const booking = 'https://edu-incpg.herokuapp.com/booking/';
-// const chats = 'https://edu-incpg.herokuapp.com/chats/'
-// const socketEndpoint = 'https://edu-incpg.herokuapp.com/';
+const BaseUrl = 'https://edu-incpg.herokuapp.com/user/';
+const assignedSubject = 'https://edu-incpg.herokuapp.com/assignedSubject/';
+const Subject = 'https://edu-incpg.herokuapp.com/subject/';
+const authentication = 'https://edu-incpg.herokuapp.com/auth/';
+const docUpload = 'https://edu-incpg.herokuapp.com/upload/';
+const messaging = 'https://edu-incpg.herokuapp.com/messaging/';
+const booking = 'https://edu-incpg.herokuapp.com/booking/';
+const chats = 'https://edu-incpg.herokuapp.com/chats/'
+const socketEndpoint = 'https://edu-incpg.herokuapp.com/';
 // const rooms = 'https://edu-incpg.herokuapp.com/rooms/';
 //-------------------------------Localhost_Urls----------------------------
-const BaseUrl = 'http://localhost:4310/user/';
-const assignedSubject = 'http://localhost:4310/assignedSubject/';
-const Subject = 'http://localhost:4310/subject/';
-const authentication = 'http://localhost:4310/auth/';
-const docUpload = 'http://localhost:4310/upload/';
-const socketEndpoint = 'http://localhost:43200/';
-const messaging = 'http://localhost:4310/messaging/';
-const booking = 'http://localhost:4310/booking/';
-const chats = 'http://localhost:4320/chats/';
+// const BaseUrl = 'http://localhost:4310/user/';
+// const assignedSubject = 'http://localhost:4310/assignedSubject/';
+// const Subject = 'http://localhost:4310/subject/';
+// const authentication = 'http://localhost:4310/auth/';
+// const docUpload = 'http://localhost:4310/upload/';
+// const socketEndpoint = 'http://localhost:43200/';
+// const messaging = 'http://localhost:4310/messaging/';
+// const booking = 'http://localhost:4310/booking/';
+// const chats = 'http://localhost:4320/chats/';
 
 export const environment = {
   production: false,
